ad4f7d8677c220f1fb2fe128810f514b57a8ee89
unknown <mdviky@gmail.com>
2018-04-27 16:47:54 +0530

    First commit

---
+++ Mextension/Block/Mextension.php
@@ -0,0 +1,10 @@
+<?php
+namespace Learn\Mextension\Block;
+ 
+class Mextension extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hi There!!';
+    }
+}
